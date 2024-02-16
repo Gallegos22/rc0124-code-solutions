8e2d74fa094a4c280c98886c3bd54ec958a51309
Eduardo Gallegos <gallegoseduardo414@gmail.com>
2024-02-16 18:36:44 +0000

    Completed typescript-and-json Exercise

---
+++ typescript-and-json/main.ts
@@ -0,0 +1,31 @@
+const myArray = [
+  {
+    isbn: '123',
+    title: 'title 1',
+    author: 'author 1',
+  },
+  {
+    isbn: '456',
+    title: 'title 2',
+    author: 'author 2',
+  },
+  {
+    isbn: '789',
+    title: 'title 3',
+    author: 'author 3',
+  },
+];
+
+console.log('myArray: ', myArray);
+console.log('typeof myArray: ', typeof myArray);
+
+console.log('JSON.stringify(myArray):', JSON.stringify(myArray));
+console.log('typeof JSON.stringify(myArray): ', typeof JSON.stringify(myArray));
+
+const student = '{"numberID":36027,"name":"Eduardo Gallegos"}';
+
+console.log('student: ', student);
+console.log('typeof student: ', typeof student);
+
+console.log('JSON.parse(student): ', JSON.parse(student));
+console.log('typeof JSON.parse(student): ', typeof JSON.parse(student));
