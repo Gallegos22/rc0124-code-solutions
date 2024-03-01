e5cb9e9198d6cac2a10690ae8f21811c5c5b38d8
Eduardo Gallegos <gallegoseduardo414@gmail.com>
2024-03-01 00:04:45 +0000

    Completed fetch Exercise

---
+++ fetch/main.ts
@@ -0,0 +1,27 @@
+async function userFunction(): Promise<void> {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+
+    const user = await response.json();
+    console.log(user);
+  } catch (error) {
+    console.log('Not a good API', error);
+  }
+}
+
+console.log(userFunction());
+
+async function favPokemon(): Promise<void> {
+  try {
+    const response = await fetch('https://pokeapi.co/api/v2/berry/cheri/');
+    if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+
+    const pokemon = await response.json();
+    console.log(pokemon);
+  } catch (error) {
+    console.log('Not a good API', error);
+  }
+}
+
+console.log(favPokemon());
