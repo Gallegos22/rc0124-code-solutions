9fbdbe8ac2c6a3776d234d235968b2cec5b79e09
Eduardo Gallegos <gallegoseduardo414@gmail.com>
2024-03-25 17:16:56 +0000

    Completed react validate input Challenge

---
+++ react-validated-input/my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Validator } from './Validator'
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Validator/>
+    </>
+  )
+}
+
+export default App
