beeca1deea9016abcbef8e2e19802ea35c63a972
Eduardo Gallegos <gallegoseduardo414@gmail.com>
2024-04-15 18:57:50 +0000

    Completed React Catalog

--- react-catalog/my-app/src/App.tsx
+++ react-catalog/my-app/src/App.tsx
@@ -1,15 +1,21 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import { About } from './pages/About';
+import { Catalog } from './pages/Catalog';
 import { Header } from './components/Header';
+import { NotFound } from './pages/NotFound';
+import { ProductDetails } from './pages/ProductDetails';
 
-function App() {
+export function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Header />} />
-        {/* <Route index element={Dashboard />} /> */}
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Catalog />} />
+        <Route path="details/:productId" element={<ProductDetails />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
